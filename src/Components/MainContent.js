c8eff552e2191f47300db031d7cc6d000a605b84
Amit Kumar <info.paradisetechsoft@gmail.com>
2019-11-02 18:11:18 +0530

    New Features of Single Page Application React 2/11/12

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,12 +1,48 @@
 import React,{Component} from 'react'
-import CardsComponent from './CardsComponent'
+import Cards from './Cards'
+import data from '../DB/Database'
+
 class MainContent extends Component{
+    constructor(props) 
+    {
+        super(props);
+        this.state={data:data}
+    }
+    ChangeStatus= (val)=>{
+        console.log(val.target.value)
+        var foundValue = data.filter(data=>data.id==val.target.value);
+        console.log(foundValue[0])
+       
+        
+    }
     render(){
-        return(
-            <div className="container">
-                    <CardsComponent />
-            </div>
+        
+    let optionsData = this.state.data.map((item, i) => {
+        return (
+            <option key={i} value={item.id}>{item.slug}</option>
         )
+    }, this);
+    let datafromdb = data.map(item=><Cards key={item.id} title={item.Title} description={item.description} image={item.image}/>)
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-4">
+                    <div className="form-group">
+                        <label>Select Cards Category</label>
+                        <select className="form-control" onChange={this.ChangeStatus} >
+                            <option value="">Select Data</option>
+                            {optionsData}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="row">
+                {datafromdb}    
+            </div>
+        </div>
+    )
     }
 }
+
 export default MainContent
--- src/DB/Database.js
+++ src/DB/Database.js
@@ -2,30 +2,28 @@ const data = [
     {
         'id':1,
         'Title':'My favourite food is pizza',
+        'slug':'pizza',
         'description':'Pizza (Italian: [ˈpittsa], Neapolitan: [ˈpittsə]) is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and various other ingredients (anchovies, olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven.[1] In formal settings, like a restaurant, pizza is eaten with knife and fork[citation needed], but in casual settings it is cut into wedges to be eaten while held in the hand. Small pizzas are sometimes called pizzettas.',
         'image':'images/pizza.jpg'
     },
     {
         'id':2,
         'Title':'My favourite food is Samosa',
+        'slug':'Samosa',
         'description':'A samosa (/səˈmoʊsə/) is a fried or baked dish with a savoury filling, such as spiced potatoes, onions, peas, meat, or lentils. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.[2][3][4] The Indian style, often accompanied by a chutney,',
         'image':'images/Samosa.jpg'
     },
     {
         'id':3,
         'Title':'My favourite food is Pasta',
+        'slug':'Pasta',
         'description':'Pasta (US: /ˈpɑːstə/, UK: /ˈpæstə/; Italian pronunciation: [ˈpasta]) is a type of food typically made from an unleavened dough of durum wheat flour (semolina) mixed with water or eggs, and formed into sheets or various shapes, then cooked by boiling or baking. Rice flour, or legumes such as beans or lentils, ',
         'image':'images/Pasta.jpg'
     },
     {
         'id':4,
         'Title':'My favourite food is Hot Dog',
-        'description':'The hot dog[2][3] (also spelled hotdog) or dog is a grilled or steamed link-sausage sandwich where the sausage is served in the slit of a partially sliced bun.[4] It can also refer to the sausage itself. The sausage used is the wiener (Vienna sausage) or frankfurter (also frank). ',
-        'image':'images/HotsDog.jpg'
-    },
-    {
-        'id':5,
-        'Title':'My favourite food is Hot Dog',
+        'slug':'HotDog',
         'description':'The hot dog[2][3] (also spelled hotdog) or dog is a grilled or steamed link-sausage sandwich where the sausage is served in the slit of a partially sliced bun.[4] It can also refer to the sausage itself. The sausage used is the wiener (Vienna sausage) or frankfurter (also frank). ',
         'image':'images/HotsDog.jpg'
     }
